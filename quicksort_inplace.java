9406472e55c84da3f55ae19d519089eb519a4d7d
Aditya <aditya.enthu@gmail.com>
2017-01-13 12:10:48 +0530

    fixed bug in quicksort

--- quicksort_inplace.java
+++ quicksort_inplace.java
@@ -7,26 +7,27 @@ public class Main {
             return;
 
         int pivot = ar[high];
+        int q = low;
         for (int i = low; i < high; i++) {
             if (ar[i] <= pivot) {
-                int temp = ar[low];
-                ar[low] = ar[i];
+                int temp = ar[q];
+                ar[q] = ar[i];
                 ar[i] = temp;
-                low++;
+                q++;
             }
         }
 
         // Put pivot in place
         int temp = ar[high];
-        ar[high] = ar[low];
-        ar[low] = temp;
+        ar[high] = ar[q];
+        ar[q] = temp;
 
-        quicksort(ar, 0, low-1);
-        quicksort(ar, low+1, high);
+        quicksort(ar, low, q-1);
+        quicksort(ar, q+1, high);
     }
 
     public static void main(String[] args) {
-        int[] ar = {18, 6, 9, 1, 4, 15, 12, 5, 6, 7, 11};
+        int[] ar = {2, 5, 3, 1};//{18, 6, 9, 1, 4, 15, 12, 5, 6, 7, 11};
 //        int[] ar = {18, 6};
         quicksort(ar, 0, ar.length-1);
         printArray(ar);
